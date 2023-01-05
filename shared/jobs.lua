6be60be7bbbb0906bfbaaa801437f0f8566748f3
jj_case-gaming <jjcasegaming@yahoo.com>
2023-01-04 21:14:36 -0800

    added bean to jobs

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -516,6 +516,34 @@ QBShared.Jobs = {
             },
         },
 	},
+    ['bean'] = {
+		label = 'Bean Machine',
+		defaultDuty = false,
+		offDutyPay = false,
+		grades = {
+            ['0'] = {
+                name = 'Novice',
+                payment = 50
+            },
+			['1'] = {
+                name = 'Employee',
+                payment = 75
+            },
+			['2'] = {
+                name = 'Experienced',
+                payment = 100
+            },
+			['3'] = {
+                name = 'Advanced',
+                payment = 125
+            },
+			['4'] = {
+                name = 'Boss',
+				isboss = true,
+                payment = 150
+            },
+        },
+	},
     ["lostrepair"] = {
         label = "Lost Repair",
         defaultDuty = false,
