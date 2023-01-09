bae96718bf1d5cbc56a99fb9711a00688902176a
jj_case-gaming <jjcasegaming@yahoo.com>
2023-01-09 06:49:00 -0800

    boii

--- server/player.lua
+++ server/player.lua
@@ -115,9 +115,7 @@ function QBCore.Player.CheckPlayerData(source, PlayerData)
     PlayerData.metadata['callsign'] = PlayerData.metadata['callsign'] or 'NO CALLSIGN'
     PlayerData.metadata['fingerprint'] = PlayerData.metadata['fingerprint'] or QBCore.Player.CreateFingerId()
     PlayerData.metadata['walletid'] = PlayerData.metadata['walletid'] or QBCore.Player.CreateWalletId()
-    PlayerData.metadata['criminalrecord'] = PlayerData.metadata['criminalrecord'] or {
-        ['hasRecord'] = false,
-        ['date'] = nil
+    PlayerData.metadata['criminalrecord'] = PlayerData.metadata['criminalrecord'] or {['hasRecord'] = false,['date'] = nil
     }
     PlayerData.metadata['licences'] = PlayerData.metadata['licences'] or {
         ['driver'] = true,
